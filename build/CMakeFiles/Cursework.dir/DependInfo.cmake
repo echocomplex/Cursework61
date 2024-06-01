dd571737105dca9fcaf8aaae40b8a80c2b3e5e30
echo complex <lexa33232@gmail.com>
2024-06-01 22:58:11 +0300

    Update

---
+++ build/CMakeFiles/Cursework.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mac/Documents/project-codes/C++/Cursework61/Student.cpp" "CMakeFiles/Cursework.dir/Student.cpp.o" "gcc" "CMakeFiles/Cursework.dir/Student.cpp.o.d"
+  "/Users/mac/Documents/project-codes/C++/Cursework61/University.cpp" "CMakeFiles/Cursework.dir/University.cpp.o" "gcc" "CMakeFiles/Cursework.dir/University.cpp.o.d"
+  "/Users/mac/Documents/project-codes/C++/Cursework61/main.cpp" "CMakeFiles/Cursework.dir/main.cpp.o" "gcc" "CMakeFiles/Cursework.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Cursework.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Cursework.
+# This may be replaced when dependencies are built.
